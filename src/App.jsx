b7567b7e2bd426c934117416785af6ddf7d668c2
Karina R <127828392+Asterose-B612@users.noreply.github.com>
2023-10-16 21:57:02 -0300

    se agrego la ruta de ProductDetail, cart, y prueba de ruta no definitiva del error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
 import './App.css'
-import Categories from './components/categories/Categories'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ProductList from './components/products/ProductList'
-import CategoriesProductList from './components/categories/CategoriesProductList'
-import Navbar from './components/Navbar/Navbar'
+import Categories from './components/ItemListContainer/Categories';
+import { BrowserRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductList from './components/products/ProductList';
+import CategoriesProductList from './components/ItemListContainer/CategoriesProductList';
+import NavBar from './components/NavBar/NavBar';
+import Nosotros from './components/Nosotros/Nosotros';
+import Contacto from './components/Contacto/Contacto';
+import Cart from './components/Cart/Cart';
+import ProductDetail from './components/products/ProductDetail';
+
+
+
+
 
 //path es el lugar a donde va a ir y cargo en element el elemento por donde quiero que comienze
 function App() {
 
   return (
     <>
-      <Router>
+      <BrowserRouter>
        
-       
-        <Navbar />
+        <NavBar/>
+
         <Routes>
-          <Route exact path='/' element={<Categories />} />
-          <Route exact path="/products" element={<ProductList />} />
-          <Route exact path="/category/:categoryId" element={<CategoriesProductList />} />
+        <Route exact path='/' element={<Categories/>} />
+          <Route exact path='/home' element={<Categories/>} />
+          <Route exact path="/products" element={<ProductList/>} />
+          <Route exact path="/category/:categoryId" element={<CategoriesProductList/>} />
+          <Route exact path="/nosotros" element={<Nosotros/>} />
+          <Route exact path="/contacto" element={<Contacto/>} />
+          <Route exact path="/cart" element={<Cart/>} />
+          <Route path="/item/:itemId" element={<ProductDetail />} />
+          <Route path="*" element={<h1 style={{ marginTop: '10vh' }}>404 NOT FOUND</h1>} />
         </Routes>
 
-      </Router>
+      </BrowserRouter>
     </>
   )
 }
